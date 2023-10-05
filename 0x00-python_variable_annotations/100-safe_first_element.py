0ae3671e147d2612835490469bb30cffb14f396b
Abdi B <abdi201380@gmail.com>
2023-10-05 19:49:21 +0300

    [Function]: Retuens first element of a list

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -8,6 +8,7 @@ from typing import List, Sequence, Union, Any
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''Returns the first element of a list or None'''
     if lst:
         return lst[0]
     else:
