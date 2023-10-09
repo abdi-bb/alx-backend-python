7120b00f74b79899c6192bc12967ed07ab7a529f
Abdi Berhe <abdiberhe@gmail.com>
2023-10-09 23:28:50 +0300

    [Update]: Avoid unused variable

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,6 +12,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     '''Spawns wait_random n times with the specified max_delay'''
     delays = []
-    for i in range(n):
+    for _ in range(n):
         delays.append(asyncio.create_task(wait_random(max_delay)))
     return [await delay for delay in asyncio.as_completed(delays)]
