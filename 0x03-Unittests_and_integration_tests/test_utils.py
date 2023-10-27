e80db49b84e9873a370c3c7531279703edf9e4c0
Abdi Berhe <abdiberhe@gmail.com>
2023-10-27 21:47:13 +0300

    [Update]: Add method to test the access_nested_map if it raises exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -23,6 +23,17 @@ class TestAccessNestedMap(unittest.TestCase):
         '''Method to test that the method returns what it is supposed to.'''
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self,
+                                         nested_map: Mapping,
+                                         path: Sequence) -> None:
+        '''Test whether the access_nested_map raises exception'''
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
 
 if __name__ == '__main__':
     unittest.main()
