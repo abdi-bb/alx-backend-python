f8599916aaac63fd1b649543c87c416a71b59b4a
Abdi Berhe <abdiberhe@gmail.com>
2023-10-30 12:50:43 +0300

    [Method]: Test method for memoize method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -42,7 +42,7 @@ class TestGetJson(unittest.TestCase):
     """Test the get_json function."""
 
     @parameterized.expand(
-            [
+        [
             ("http://example.com", {"payload": True}),
             ("http://holberton.io", {"payload": False}),
         ]
@@ -56,6 +56,30 @@ class TestGetJson(unittest.TestCase):
         mock_get.return_value = test_payload
         result = utils.get_json(test_url)
         self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    '''Class to test utils.memoize'''
+
+    def test_memoize(self):
+        '''Test the a_property result and it's called twice
+         a_method is called once '''
+
+        class TestClass:
+            '''Nested class'''
+
+            def a_method(self):
+                return 42
+
+            @utils.memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method") as mock_a_method:
+            test_class = TestClass()
+            test_class.a_property
+            test_class.a_property
+            mock_a_method.assert_called_once
 
 
 if __name__ == '__main__':
